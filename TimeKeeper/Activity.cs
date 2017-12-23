4bb46ab4527ebece6b53eabc1e007f0f0deb0558
jellesse92 <jehunter@uci.edu>
2017-12-23 11:30:43 -0800

    Adding base files

---
+++ TimeKeeper/Activity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inTime.TimeKeeper
+{
+    public struct ActivityEvent
+    {
+        DateTime start;
+        DateTime stop;
+    }
+    class Activity
+    {
+        public string name;
+        public string category;
+
+        protected ActivityEvent[] events;
+
+        private Guid id;
+        private Guid user;
+
+
+    }
+}
---
+++ TimeKeeper/TimeKeeper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inTime.TimeKeeper
+{
+    class TimeKeeper
+    {
+    }
+}
