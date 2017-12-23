185974861f8eddf082627116d7ddd9295e6447bf
jellese92 <jehunter@uci.edu>
2017-12-23 15:28:29 -0800

    activity class

--- TimeKeeper/Activity.cs
+++ TimeKeeper/Activity.cs
@@ -8,18 +8,69 @@ namespace inTime.TimeKeeper
 {
     public struct ActivityEvent
     {
-        DateTime start;
-        DateTime stop;
+        public DateTime start { get; set; } 
+        public DateTime stop { get; set; }
     }
+
     class Activity
     {
         public string name;
         public string category;
 
-        protected ActivityEvent[] events;
+        protected Stack<ActivityEvent> events;
 
         private Guid id;
         private Guid user;
+
+        public Activity(string name ="", string category = "")
+        {
+            this.name = name;
+            this.category = category;
+        }
+
+        public void changeActivityName(string name)
+        {
+            this.name = name;
+        }
+
+        public void setActivityCategory(string category)
+        {
+            this.category = category;
+        }
+
+        public ActivityEvent getLastEvent()
+        {
+           return events.Peek();
+        }
+
+        public void cancelEvent()
+        {
+            events.Pop();
+        }
+
+        public bool createEvent()
+        {
+            if (events.Count == 0 || events.Peek().stop > events.Peek().start)
+            {
+                ActivityEvent newEvent = new ActivityEvent();
+                newEvent.start = DateTime.Now;
+                events.Push(newEvent);
+                return true;
+            }
+            return false;
+        }
+
+        public bool endEvent()
+        {
+            if (events.Count > 0 && events.Peek().stop < events.Peek().start)
+            {
+                ActivityEvent currentEvent = events.Pop();
+                currentEvent.stop = DateTime.Now;
+                events.Push(currentEvent);
+                return true;
+            }
+            return false;
+        }
 
 
     }
